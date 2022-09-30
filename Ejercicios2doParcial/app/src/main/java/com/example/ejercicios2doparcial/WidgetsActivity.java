0e4e37d710b6b9d4f9413f0a516286fb238a44cf
Diego A. Medina <79292091+archivogogo@users.noreply.github.com>
2022-09-29 19:27:09 -0500

    Version 1.0
    Initial Commit

---
+++ Ejercicios2doParcial/app/src/main/java/com/example/ejercicios2doparcial/WidgetsActivity.java
@@ -0,0 +1,14 @@
+package com.example.ejercicios2doparcial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WidgetsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_widgets);
+    }
+}
